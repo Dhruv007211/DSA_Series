32ac46c3868be91fa9fd975d42fef784e2a395bd
Dhruv Singh <dhruv25072006@gmail.com>
2026-07-17 21:01:15 +0530

    Time: 0 ms (100%), Space: 43.7 MB (62.69%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,23 +1,42 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i=m-1;
-        int j=n-1;
-        int k=m+n-1;
-        while(i>=0&&j>=0){
-            if(nums1[i]<nums2[j]){
-                nums1[k]=nums2[j];
-                j--;
+
+        int[] temp = new int[m + n];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        // Merge both arrays
+        while (i < m && j < n) {
+
+            if (nums1[i] <= nums2[j]) {
+                temp[k] = nums1[i];
+                i++;
+            } else {
+                temp[k] = nums2[j];
+                j++;
             }
-            else{
-                nums1[k]=nums1[i];
-                i--;
-            }
-            k--;
+            k++;
         }
-        while(j>=0){
-             nums1[k]=nums2[j];
-                j--;
-                k--;
+
+        // Remaining elements of nums1
+        while (i < m) {
+            temp[k] = nums1[i];
+            i++;
+            k++;
+        }
+
+        // Remaining elements of nums2
+        while (j < n) {
+            temp[k] = nums2[j];
+            j++;
+            k++;
+        }
+
+        // Copy back to nums1
+        for (int x = 0; x < m + n; x++) {
+            nums1[x] = temp[x];
         }
     }
 }
