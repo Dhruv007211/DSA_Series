6942b42434038f76d71a8f1b100329b8620553cf
Dhruv Singh <dhruv25072006@gmail.com>
2026-08-31 10:25:53 +0530

    Time: 2 ms (99.81%), Space: 45.4 MB (88.95%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,37 +1,28 @@
 class Solution {
     public String minWindow(String s, String t) {
-
-        String ans = "";
-
-        int[] freq = new int[128];
-
-        for(char c : t.toCharArray())
-            freq[c]++;
-
-        int j = 0;
-        int count = t.length();
-
-        for(int i = 0; i < s.length(); i++){
-
-            if(freq[s.charAt(i)] > 0)
-                count--;
-
-            freq[s.charAt(i)]--;
-
-            while(count == 0){
-
-                if(ans.equals("") || ans.length() > (i - j + 1))
-                    ans = s.substring(j, i + 1);
-
-                freq[s.charAt(j)]++;
-
-                if(freq[s.charAt(j)] > 0)
-                    count++;
-
-                j++;
-            }
+        int need[]=new int [128];
+        for(char c:t.toCharArray()){
+            need[c]++;
         }
-
-        return ans;
+        int start=0;
+        int left=0;
+        int min=Integer.MAX_VALUE;
+        int count=t.length();
+for(int r=0;r<s.length();r++){
+       char c1=s.charAt(r);
+    if(need[c1]>0)count--;
+    need[c1]--;
+    while(count==0){
+        if(r-left+1<min){
+            min=r-left+1;
+            start=left;
+        }
+        char c=s.charAt(left);
+          need[c]++;
+        if(need[c]>0)count++;
+        left++;
+    }
+}
+return min==Integer.MAX_VALUE?"":s.substring(start,start+min);
     }
 }
