0fc4be98d179b463d90acb498ab5356208c5397d
Dhruv Singh <dhruv25072006@gmail.com>
2026-09-05 14:12:25 +0530

    Time: 119 ms (93.05%), Space: 43 MB (59.87%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -5,20 +5,21 @@ class Solution {
         boolean[][]vis=new boolean[r][c];
        for(int i=0;i<r;i++){
         for(int j=0;j<c;j++){
-            if(find(board,word,i,j,0,vis))return true;
+            if(find(board,word,i,j,0))return true;
         }
        }
        return false;
     }
-    boolean find(char[][]board,String word,int i,int j,int idx,boolean[][]vis){
+    boolean find(char[][]board,String word,int i,int j,int idx){
         if(idx==word.length())return true;
-        if(i<0||j<0||i>=board.length||j>=board[0].length||vis[i][j]||board[i][j]!=word.charAt(idx))return false;
-        vis[i][j]=true;
-      if (find(board,word,i+1,j,idx+1,vis)||
-        find(board,word,i,j+1,idx+1,vis)||
-        find(board,word,i-1,j,idx+1,vis)||
-        find(board,word,i,j-1,idx+1,vis))return true;
-        vis[i][j]=false;
+        if(i<0||j<0||i>=board.length||j>=board[0].length||board[i][j]!=word.charAt(idx))return false;
+        char temp=board[i][j];
+        board[i][j]='#';
+      if (find(board,word,i+1,j,idx+1)||
+        find(board,word,i,j+1,idx+1)||
+        find(board,word,i-1,j,idx+1)||
+        find(board,word,i,j-1,idx+1))return true;
+       board[i][j]=temp;
         return false;
     }
 }
