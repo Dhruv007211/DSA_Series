13a6510e5e1a440ccb52c7cc2e81ac4e13f0edc4
Dhruv Singh <dhruv25072006@gmail.com>
2026-09-07 19:09:41 +0530

    Time: 2 ms (81.43%), Space: 46.2 MB (97.44%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,81 +1,54 @@
 class Solution {
-    List<List<String>> ans = new ArrayList<>();
-
+    List<List<String>>ans=new ArrayList<>();
     public List<List<String>> solveNQueens(int n) {
-
-        char[][] board = new char[n][n];
-
-        for(int i = 0; i < n; i++) {
-            Arrays.fill(board[i], '.');
+        char[][]board=new char[n][n];
+        for(int i=0;i<n;i++){
+            Arrays.fill(board[i],'.');
         }
-
-        solve(board, 0);
-
+        solve(board,0);
         return ans;
     }
-
-    void solve(char[][] board, int row) {
-
-        if(row == board.length) {
+    void solve(char[][]board,int row){
+        if(row==board.length){
             ans.add(make(board));
-            return;
+            return ;
         }
+        for(int col=0;col<board.length;col++){
+        if(safe(board,row,col)){
+board[row][col]='Q';
+solve(board,row+1);
+board[row][col]='.';
 
-        for(int c = 0; c < board.length; c++) {
-
-            if(safe(board, row, c)) {
-
-                board[row][c] = 'Q';
-
-                solve(board, row + 1);
-
-                board[row][c] = '.';
-            }
         }
-    }
-
-    boolean safe(char[][] board, int row, int col) {
-
-        int n = board.length;
-
-        // Same column
-        for(int i = 0; i < row; i++) {
-            if(board[i][col] == 'Q') {
-                return false;
-            }
-        }
-
-        // Upper-left diagonal
-        for(int i = row - 1, j = col - 1;
-            i >= 0 && j >= 0;
-            i--, j--) {
-
-            if(board[i][j] == 'Q') {
-                return false;
-            }
-        }
-
-        // Upper-right diagonal
-        for(int i = row - 1, j = col + 1;
-            i >= 0 && j < n;
-            i--, j++) {
-
-            if(board[i][j] == 'Q') {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    List<String> make(char[][] board) {
-
-        List<String> list = new ArrayList<>();
-
-        for(char[] row : board) {
-            list.add(new String(row));
-        }
-
-        return list;
     }
 }
+boolean safe(char[][]board,int row,int col){
+    int n=board.length;
+    for(int i=0;i<n;i++){
+        if(board[i][col]=='Q')return false;
+    }
+  for(int i = row - 1, j = col - 1;
+             i >= 0 && j >= 0;
+             i--, j--) {
+
+             if(board[i][j] == 'Q') {
+                 return false;
+             }
+         }
+         for(int i = row - 1, j = col + 1;
+             i >= 0 && j <n;
+             i--, j++) {
+
+             if(board[i][j] == 'Q') {
+                 return false;
+             }
+         }
+         return true;
+}
+List<String>make(char[][]board){
+List<String>list=new ArrayList<>();
+for(char []row:board){
+    list.add(new String(row));
+}
+return list;
+}}
